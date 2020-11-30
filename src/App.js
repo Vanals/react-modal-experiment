8e4937edbff1c03754c5581fc0289d745a6b99c4
Vanals <marco91vanali@gmail.com>
2020-11-30 11:48:21 +0000

    applied react modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ModalMe from './ModalMe';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <ModalMe />
+
+      <div id="marco"></div>
     </div>
   );
 }
